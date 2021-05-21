10e2af68427b59651ed5f1964a8bedffd39b94f0
Solan Delvenne <delvennesolan@gmail.com>
2021-05-21 14:30:36 +0200

    Restructure some code

--- utils/game.py
+++ utils/game.py
@@ -1,5 +1,25 @@
 from typing import List, Any
 from random import choice
+import platform  # For getting the operating system name
+import os
+
+parts = ["  +---+\n  |   |\n      |\n      |\n      |\n      |\n=========",
+         "  +---+\n  |   |\n  O   |\n      |\n      |\n      |\n=========",
+         "  +---+\n  |   |\n  O   |\n  |   |\n      |\n      |\n=========",
+         "  +---+\n  |   |\n  O   |\n /|\\  |\n      |\n      |\n=========",
+         "  +---+\n  |   |\n  O   |\n /|\\  |\n /    |\n      |\n=========",
+         "  +---+\n  |   |\n  O   |\n /|\\  |\n / \\  |\n      |\n========="]
+
+
+def clear_screen():
+    """
+    Clears the terminal screen.
+    :return None
+    """
+    # Check which OS the user is running in order to execute the correct clear
+    command = "cls" if platform.system().lower() == "windows" else "clear"
+    # Execute the clear command
+    os.system(command)
 
 
 def transpose_element(first_list: List[str],
@@ -12,6 +32,7 @@ def transpose_element(first_list: List[str],
     :param element: Element to find and transpose
     :return: None
     """
+    # Build a list to store the position of all occurrences of element
     indices = [i for i, x in enumerate(first_list) if x == element]
     for index in indices:
         second_list[index] = first_list[index]
@@ -43,9 +64,10 @@ class Hangman:
         if the game is over due to too many errors or not
     Methods
     -------
-    play()
+    start_game()
         Initializes the Hangman game and starts the game loop.
     """
+
     def __init__(self):
         self.possible_words: List[str] = ['becode',
                                           'learning',
@@ -63,12 +85,13 @@ class Hangman:
         self.error_count: int = 0
         self.is_over = False
 
-    def play(self):
+    def __play(self):
         """
         Core mechanics of the game. Take a single input from the player.
         :return None
         """
         guess: str = input("\nEnter a single letter: ")
+        clear_screen()  # Clear the screen to refresh it
         if len(guess) != 1:
             print("Invalid input, try again.")
             return
@@ -78,6 +101,7 @@ class Hangman:
             print("You've already submitted this letter. Try again.")
             return
         elif guess in self.word_to_find:
+            print("Nice guess !")
             transpose_element(
                 self.word_to_find,
                 self.correctly_guessed_letters,
@@ -96,29 +120,32 @@ class Hangman:
         """
         self.word_to_find = list(choice(self.possible_words).upper())
         self.correctly_guessed_letters = ["_"] * len(self.word_to_find)
+        clear_screen()  # Clear the screen to prettify the game
+        print("Welcome to the Hangman, will you guess the word ?")
         while self.is_over is False:
+            print(parts[self.error_count])
+            print(f"Word: {''.join(self.correctly_guessed_letters)}")
+            print(f"Incorrect guesses: "
+                  f"{', '.join(self.wrongly_guessed_letters)}")
             if self.lives == 0:
-                self.game_over()
+                self.__game_over()
                 break
             elif "_" not in self.correctly_guessed_letters:
-                self.well_played()
+                self.__well_played()
                 break
             else:
                 self.turn_count += 1
-                self.play()
-                print(f"Word: {''.join(self.correctly_guessed_letters)}")
-                print(f"Incorrect guesses: "
-                      f"{', '.join(self.wrongly_guessed_letters)}")
+                self.__play()
 
-    def game_over(self):
+    def __game_over(self):
         """
         Print the game over message.
         :return None
         """
-        print("Game over...")
+        print(f"Game over... The word was {''.join(self.word_to_find)}")
         self.is_over = True
 
-    def well_played(self):
+    def __well_played(self):
         """
         Print the success message.
         :return None
